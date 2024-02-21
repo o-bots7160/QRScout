2f49597117c571b7dd757768ba6b88c8347ee686
jrl2222 <jlcomputersolutions.jl@gmail.com>
2024-02-21 15:00:52 -0500

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -95,7 +95,9 @@ export function App() {
               >
                 Commit
               </button>
-              <p>DON'T HIT RESET UNLESS THE QR CODE HAS BEEN SCANNED!!</p>
+              <p> </p>
+              <p> </p>
+              DON'T HIT RESET UNLESS THE QR CODE HAS BEEN SCANNED!!
               <button
                 className="focus:shadow-outline mx-2 my-6 rounded border border-red-rhr bg-white py-2 font-bold uppercase text-red-rhr hover:bg-red-200 focus:outline-none dark:bg-gray-500 dark:text-white dark:hover:bg-gray-700"
                 type="button"
